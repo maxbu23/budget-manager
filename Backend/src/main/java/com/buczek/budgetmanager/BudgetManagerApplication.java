6f3cc0118399e456aba360439a6a9853dd6a96ce
maxbu <max.buczek>
2022-10-12 21:45:55 +0200

    Commit

--- Backend/src/main/java/com/buczek/budgetmanager/BudgetManagerApplication.java
+++ Backend/src/main/java/com/buczek/budgetmanager/BudgetManagerApplication.java
@@ -1,13 +1,41 @@
 package com.buczek.budgetmanager;
 
+import com.buczek.budgetmanager.model.Expense;
+import com.buczek.budgetmanager.model.ExpenseType;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BudgetManagerApplication {
 
+	@Autowired
+	private ExpenseRepository repo;
 	public static void main(String[] args) {
+
 		SpringApplication.run(BudgetManagerApplication.class, args);
+	}
+
+	@Bean
+	InitializingBean sendDatabase() {
+		return () -> {
+			repo.save(Expense.builder()
+					.name("Ksiazki")
+					.expenseType(ExpenseType.EDUCATION)
+					.amount(BigDecimal.TEN)
+					.description("")
+					.build());
+			repo.save(Expense.builder()
+					.name("Kino")
+					.expenseType(ExpenseType.ENTERTAINMENT)
+					.amount(BigDecimal.valueOf(25.))
+					.description("")
+					.build());
+		};
 	}
 
 }
--- Backend/src/main/java/com/buczek/budgetmanager/model/Expense.java
+++ Backend/src/main/java/com/buczek/budgetmanager/model/Expense.java
@@ -2,22 +2,27 @@ package com.buczek.budgetmanager.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
+@Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Expense {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "expense_type")
     private ExpenseType expenseType;
+    @Column(name = "amount")
     private BigDecimal amount;
+    @Column(name = "description")
     private String description;
 
 }
